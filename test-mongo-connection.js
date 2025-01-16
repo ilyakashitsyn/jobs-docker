4fcc26295a540352796139f95adfc45bfe8534ae
Ilya Kashitsyn <ilyakashitsyn@gmail.com>
2025-01-16 17:50:50 -0300

    added mongo local test

---
+++ test-mongo-connection.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+// Укажите свои данные для подключения
+const username = "ilyakashitsyn"; // Замените на имя пользователя из MongoDB
+const password = "FzcMNYp2XlfXEnj3"; // Замените на ваш пароль
+const clusterUrl = "cluster0.fhj1j.mongodb.net"; // Адрес вашего кластера
+const dbName = "jobs-docker"; // Название вашей базы данных
+
+// Формируем строку подключения
+const uri = `mongodb+srv://${username}:${password}@${clusterUrl}/${dbName}?retryWrites=true&w=majority`;
+
+// Подключаемся к MongoDB
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("✅ Успешное подключение к MongoDB:", dbName);
+    mongoose.disconnect(); // Закрываем подключение
+  })
+  .catch((err) => {
+    console.error("❌ Ошибка подключения к MongoDB:", err.message);
+    process.exit(1);
+  });
